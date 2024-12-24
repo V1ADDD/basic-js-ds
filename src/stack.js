d584efcd438bf2ecd4670df05f5553af32894dae
V1ADDD <rusakvlad2@gmail.com>
2024-12-24 10:24:19 +0300

    feat: stack

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.stack = [];
   }
-
-  pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  push(element) {
+    this.stack.push(element);
   }
-
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.stack.length === 0) {
+      return undefined;
+    }
+    return this.stack[this.stack.length - 1];
+  }
+  pop() {
+    if (this.stack.length === 0) {
+      return undefined;
+    }
+    return this.stack.pop();
   }
 }
 
